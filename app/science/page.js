d7217d35a0bea6f27ecae5a43f242d18d99650a3
anabarion <anajulia_barion@hotmail.com>
2025-03-24 17:14:47 -0400

    inhibitorEngager page

--- app/science/page.js
+++ app/science/page.js
@@ -1,6 +1,5 @@
 import Footer from "../../components/layout/Footer";
 import Header from "../../components/layout/Header";
-import Image from "next/image";
 
 export default function SciencePage() {
   return (
