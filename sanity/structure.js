28cdcf5678106e89e4837c340e1c0e3424e850d0
anabarion <anajulia_barion@hotmail.com>
2025-04-29 23:33:57 -0400

    Undid changes to testing branch.

--- sanity/structure.js
+++ sanity/structure.js
@@ -1,22 +1,5 @@
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-// sanity/structure.js
 export const structure = (S) =>
   S.list()
     .title('Content')
-    .items([
-      S.listItem()
-        .title('Science Page')
-        .schemaType('sciencePage')
-        .child(
-          S.document()
-            .schemaType('sciencePage')
-            .documentId('sciencePage')
-        ),
-
-      S.divider(),
-
-      ...S.documentTypeListItems().filter(
-        listItem =>
-          !['header', 'sciencePage'].includes(listItem.getId())
-      )
-    ])
+    .items(S.documentTypeListItems())
