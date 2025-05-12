d4264c066cc70cddbddb792ef324c85c1f01cc39
tyedou <tyedou3@gmail.com>
2025-05-12 16:13:40 -0400

    spaces between imports

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,14 @@
 "use client";
 import { useState, useEffect } from "react";
+
 import { client } from "../../sanity/lib/client";
+
 import { urlFor } from "../../sanity/lib/image";
+
 import Image from "next/image";
 
 import Footer from "../../components/layout/Footer";
+
 import Header from "../../components/layout/Header";
 
 export default function ContactPage() {
